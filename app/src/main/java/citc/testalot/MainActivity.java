3055a79bf7c9cede8718f4b1ee2980a5a56805af
timothyjc <timothyjc@gmail.com>
2015-06-27 08:57:58 +0100

    .

---
+++ app/src/main/java/citc/testalot/MainActivity.java
@@ -0,0 +1,215 @@
+package citc.testalot;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CoordinatorLayout cl = (CoordinatorLayout)findViewById(R.id.main_content);
+//        cl.setPadding(0,getStatusBarHeight(),0,getNavigationBarHeight());
+        Log.i("!!!!!!!!!!!!!", "status: " + getStatusBarHeight() + ", nav: " + getNavigationBarHeight());
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+
+        // As we're using a Toolbar, we should retrieve it and set it
+        // to be our ActionBar
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+//        setSupportActionBar(toolbar);
+
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final AppBarLayout appbar = (AppBarLayout) findViewById(R.id.appbar);
+
+        final RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerview);
+        setupRecyclerView(rv);
+//        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                Log.i("!!!!!", "scroll:" + rv.computeVerticalScrollOffset());
+//                if (rv.computeVerticalScrollOffset() == 0) {
+////                    getSupportActionBar().setElevation(0);
+////                    toolbar.setElevation(0);
+////                    toolbar.setZ(0);
+//                    appbar.setElevation(0);
+////                    appbar.setZ(0);
+//                } else {
+//                    appbar.setElevation(10);
+//                }
+//            }
+//
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//            }
+//        });
+
+
+
+        // Now retrieve the DrawerLayout so that we can set the status bar color.
+        // This only takes effect on Lollipop, or when using translucentStatusBar
+        // on KitKat.
+
+    }
+
+    private List<String> getRandomSublist(String[] array, int amount) {
+        ArrayList<String> list = new ArrayList<>(amount);
+        Random random = new Random();
+        while (list.size() < amount) {
+            list.add(array[random.nextInt(array.length)]);
+        }
+        return list;
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(new SimpleStringRecyclerViewAdapter(this,
+                getRandomSublist(Cheeses.sCheeseStrings, 30)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public int getNavigationBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public static class SimpleStringRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleStringRecyclerViewAdapter.ViewHolder> {
+
+        private final TypedValue mTypedValue = new TypedValue();
+        private int mBackground;
+        private List<String> mValues;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public String mBoundString;
+
+            public final View mView;
+            public final ImageView mImageView;
+            public final TextView mTextView;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mImageView = (ImageView) view.findViewById(R.id.avatar);
+                mTextView = (TextView) view.findViewById(android.R.id.text1);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mTextView.getText();
+            }
+        }
+
+        public String getValueAt(int position) {
+            return mValues.get(position);
+        }
+
+        public SimpleStringRecyclerViewAdapter(Context context, List<String> items) {
+            context.getTheme().resolveAttribute(R.attr.selectableItemBackground, mTypedValue, true);
+            mBackground = mTypedValue.resourceId;
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+            view.setBackgroundResource(mBackground);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mBoundString = mValues.get(position);
+            holder.mTextView.setText(mValues.get(position));
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Context context = v.getContext();
+//                    Intent intent = new Intent(context, CheeseDetailActivity.class);
+//                    intent.putExtra(CheeseDetailActivity.EXTRA_NAME, holder.mBoundString);
+//
+//                    context.startActivity(intent);
+                }
+            });
+
+            Glide.with(holder.mImageView.getContext())
+                    .load(Cheeses.getRandomCheeseDrawable())
+                    .fitCenter()
+                    .into(holder.mImageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+    }
+
+}
