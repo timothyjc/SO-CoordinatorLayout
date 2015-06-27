e17adad46e16c2395f6cdd52eed35d77dbaa69a0
timothyjc <timothyjc@gmail.com>
2015-06-27 09:06:14 +0100

    .

--- app/src/main/java/citc/testalot/MainActivity.java
+++ app/src/main/java/citc/testalot/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
 //        setSupportActionBar(toolbar);
 
-//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
