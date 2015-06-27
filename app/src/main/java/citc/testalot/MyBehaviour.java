343b5006a9f0ad03b192d40fe15d88d0a733d2ac
timothyjc <timothyjc@gmail.com>
2015-06-27 08:56:04 +0100

    Initial commit

---
+++ app/src/main/java/citc/testalot/MyBehaviour.java
@@ -0,0 +1,11 @@
+package citc.testalot;
+
+import android.support.design.widget.AppBarLayout;
+
+public class MyBehaviour extends AppBarLayout.ScrollingViewBehavior {
+
+    @Override
+    public int getOverlayTop() {
+        return 0;
+    }
+}
